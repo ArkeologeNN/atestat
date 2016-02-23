b6c18a4d714e1908857330e85b89fc152b0fd12f
malaiemil14 <malai.emy.s@gmail.com>
2016-02-23 21:11:30 +0200

    adaugat gallery... in sfarsit

--- scripts/script.js
+++ scripts/script.js
@@ -21,4 +21,22 @@
 			aboutDialog = true;
 		}
 	});
+
+	$('#gallery').on('click', function() {
+		$('#gallery-dialog').fadeTo(400, 1);
+	});
+
+	$('#close-control').on('click', function() {
+		$('#gallery-dialog').fadeTo(200, 0, function() {
+			$('#gallery-dialog').hide();
+		});
+	});
+
+	$('.controls').on('mouseenter', function() {
+		$(this).fadeTo(200, .7);
+	});
+
+	$('.controls').on('mouseleave', function() {
+		$(this).fadeTo(200, .3);
+	});
 });
