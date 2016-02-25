ec4a8ae5afd6b965c75de065e1b2ef33f0f32584
malaiemil14 <malai.emy.s@gmail.com>
2016-02-25 08:18:44 +0200

    aranjat scripts

---
+++ scripts/jquery.js
@@ -0,0 +1,45 @@
+﻿$(function() {
+
+	// menu indicators fading
+	$('.nav-item a').on('mouseenter', function() {
+		var itemID = $(this).attr('data-ind');
+		$("#"+itemID).fadeTo(100, 1);
+	});
+
+	$('.nav-item a').on('mouseleave', function() {
+		var itemID = $(this).attr('data-ind');
+		$("#" + itemID).fadeTo(50, 0);
+	});
+
+	var aboutDialog = true;
+	$('#about').on('click', function() {
+		if (aboutDialog === true) {
+			$('#about-dialog').fadeTo(200, 1);
+			aboutDialog = false;
+		} else {
+			$('#about-dialog').fadeTo(200, 0);
+			aboutDialog = true;
+		}
+	});
+
+	/* open gallery*/
+	$('#gallery').on('click', function() {
+		$('#gallery-dialog').fadeTo(400, 1);
+	});
+
+	/* close gallery */
+	$('#close-control').on('click', function() {
+		$('#gallery-dialog').fadeTo(200, 0, function() {
+			$('#gallery-dialog').hide();
+		});
+	});
+	 /* gallery buttons fading */
+	$('.controls').on('mouseenter', function() {
+		$(this).fadeTo(200, .8);
+	});
+
+	$('.controls').on('mouseleave', function() {
+		$(this).fadeTo(200, .5);
+	});
+});
+
